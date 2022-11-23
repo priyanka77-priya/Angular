f4e2bf09a191fbfdd18273fbc5a4e1e8f52b8e71
Priyanka <84381308+priyanka77-priya@users.noreply.github.com>
2022-11-23 20:16:39 +0530

    Create App.module.ts

---
+++ App.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+@NgModule({
+declarations: [
+
+6
+
+AppComponent
+],
+imports: [
+BrowserModule,
+AppRoutingModule
+],
+providers: [],
+bootstrap: [AppComponent]
+})
+export class AppModule { }
